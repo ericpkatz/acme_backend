1db8edfd81cdae016f907ff3b4e4dd838b7c453b
ericpkatz <ericpkatz@gmail.com>
2023-09-18 12:37:50 -0400

    setup server

---
+++ index.js
@@ -0,0 +1,43 @@
+const pg = require('pg');
+const client = new pg.Client('postgres://localhost/acme_backend_db');
+const express = require('express');
+const app = express();
+
+app.get('/api/things', async(req, res, next)=> {
+  try {
+    const SQL = `
+      SELECT *
+      FROM things
+    `;
+    const response = await client.query(SQL);
+    res.send(response.rows);
+  }
+  catch(ex){
+    next(ex);
+  }
+});
+
+const setup = async()=> {
+  await client.connect();
+  console.log('connected to the database');
+  const SQL = `
+    DROP TABLE IF EXISTS things;
+    CREATE TABLE things(
+      id SERIAL PRIMARY KEY,
+      name VARCHAR(100)
+    );
+    INSERT INTO things (name) VALUES ('foo');
+    INSERT INTO things (name) VALUES ('bar');
+    INSERT INTO things (name) VALUES ('bazz');
+    INSERT INTO things (name) VALUES ('quq');
+  `;
+  await client.query(SQL);
+  console.log('tables created and data seeded');
+
+  const port = process.env.PORT || 3000;
+  app.listen(port, ()=> {
+    console.log(`listening on port ${port}`);
+  });
+};
+
+setup();
